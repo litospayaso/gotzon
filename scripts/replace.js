42e535d2d1216837fd9c6f29c472b144a79536c3
Angel Hita <a.hita@talmundo.com>
2021-12-11 18:54:52 +0100

    updating scripts

--- scripts/replace.js
+++ scripts/replace.js
@@ -3,7 +3,7 @@ fs.readFile('www/index.html', 'utf8', function (err,data) {
   if (err) {
     return console.log(err);
   }
-  const result = data.replace('<base href="/" />', '<base href="https://litospayaso.github.io/tato/" />');
+  const result = data.replace('<base href="/" />', '<base href="https://litospayaso.github.io/gotzon/" />');
 
   fs.writeFile('www/index.html', result, 'utf8', function (err) {
      if (err) return console.log(err);
